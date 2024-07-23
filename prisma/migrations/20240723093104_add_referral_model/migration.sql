ae86fafe2a3b723571ded7b25297d71dae914735
Anas Karatela <anas.karatela@gmail.com>
2024-07-23 15:01:54 +0530

    fix sql

--- prisma/migrations/20240723093104_add_referral_model/migration.sql
+++ prisma/migrations/20240723093104_add_referral_model/migration.sql
@@ -1,11 +1,9 @@
 -- CreateTable
 CREATE TABLE `Referral` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `refereeEmail` VARCHAR(191) NOT NULL,
     `refereeName` VARCHAR(191) NOT NULL,
     `referrerName` VARCHAR(191) NOT NULL,
     `referrerEmail` VARCHAR(191) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`refereeEmail`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
