020dcfe9e9f7449256462259dde5b4017278d4eb
Anas Karatela <anas.karatela@gmail.com>
2024-07-26 00:50:05 +0530

    fix backend issue 1

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ require('dotenv').config();
 
 const app = express();
 const prisma = new PrismaClient();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // app.use(cors({origin:"https://refer-and-earn-ah0jf4k0j-anas-karatelas-projects.vercel.app/"}));
+// const corsOptions ={
+//   origin:'http://localhost:3000', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
 app.use(cors());
+
 app.use(express.json());
 
 app.post('/api/referral', async (req, res) => {
